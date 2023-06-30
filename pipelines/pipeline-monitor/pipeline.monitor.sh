ff8cfbeaf29476009667df02d68e0b988667fad5
LorraineGomes <lorraine@crowtech.digital>
2023-06-30 17:43:36 -0400

    Pipeline monitor

---
+++ pipelines/pipeline-monitor/pipeline.monitor.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Pipeline Monitor.
+store_array+=("monitor")
+
+    for i in "${store_array[@]}"
+    do
+        # Diretório para percorrer.
+        TARGET="/var/www/html/$i"
+
+        echo " | Rodando composer install          |"
+        echo " |===================================/"
+        cd /var/www/html/$i
+        composer install
+        echo " /===================================|"
+        echo " | Executando Dump-Autoload..."
+        echo " |===================================/"
+        composer dump-autoload
+        echo " /===================================|"
+        echo " /===================================|"
+        echo " | Mudando pra Branch main|"
+        echo " |===================================/"
+        cd $TARGET
+        git checkout main
+        echo " | Executando as Migrations|"
+        echo " |===================================/"
+        php artisan migrate
+        echo " /===================================|"
+        echo " | Alterando permissões de pastas..."
+        echo " |===================================/"
+        chown -R www-data:dev .
+        chmod -R 755 .
+        chmod -R 775 storage/
+        chmod -R 775 bootstrap/cache/
+        echo " /===================================|"
+        echo " | Deploy no Ambiente Stages..."
+        git pull origin main
+        echo " |===================================/"
+        echo " /===================================|"
+        echo " | Concluído... =)"
+        echo " |===================================/"
+        exit
+    done
